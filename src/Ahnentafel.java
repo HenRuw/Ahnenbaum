705c7469ec924e7aa5cd47f3769e52b4ca5473a0
henning.ruwwe <henning4454@gmail.com>
2023-03-03 13:02:07 +0100

    Order systeme

--- src/Ahnentafel.java
+++ src/Ahnentafel.java
@@ -1,3 +1,5 @@
+import jdk.jfr.ContentType;
+
 public class Ahnentafel {
 
     private BinaryTree<Person> ahnenbaum;
@@ -5,8 +7,56 @@ public class Ahnentafel {
         ahnenbaum = new BinaryTree();
     }
 
+    public Ahnentafel(BinaryTree<Person> ahnenbaum) {
+        this.ahnenbaum = ahnenbaum;
+    }
+
     public BinaryTree<Person> getAhnenbaum() {
         return ahnenbaum;
     }
+
+    public String preOrder(BinaryTree<Person> pTree){
+        String out = "";
+        if(pTree.getContent() != null){
+            out += pTree.getContent().getVorname() + " " + pTree.getContent().getNachname() + " Geschlecht: " + pTree.getContent().getGeschlecht() + "\n";
+        }
+        if (!pTree.getLeftTree().isEmpty()){
+            out += preOrder(pTree.getLeftTree());
+        }
+        if (!pTree.getRightTree().isEmpty()) {
+            out += preOrder(pTree.getRightTree());
+        }
+        return out;
+    }
+
+    public String InOrder(BinaryTree<Person> pTree){
+        String out = "";
+        if (!pTree.getLeftTree().isEmpty()){
+            out += preOrder(pTree.getLeftTree());
+        }
+        if(pTree.getContent() != null){
+            out += pTree.getContent().getVorname() + " " + pTree.getContent().getNachname() + " Geschlecht: " + pTree.getContent().getGeschlecht() + "\n";
+        }
+        if (!pTree.getRightTree().isEmpty()) {
+            out += preOrder(pTree.getRightTree());
+        }
+        return out;
+    }
+
+    public String postOrder(BinaryTree<Person> pTree){
+        String out = "";
+        if (!pTree.getLeftTree().isEmpty()){
+            out += preOrder(pTree.getLeftTree());
+        }
+        if (!pTree.getRightTree().isEmpty()) {
+            out += preOrder(pTree.getRightTree());
+        }
+        if(pTree.getContent() != null){
+            out += pTree.getContent().getVorname() + " " + pTree.getContent().getNachname() + " Geschlecht: " + pTree.getContent().getGeschlecht() + "\n";
+        }
+        return out;
+    }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Ahnentafel t = new Ahnentafel();
-        BinaryTree b = new BinaryTree<>(new BinaryTree<>(),new BinaryTree<>(), new BinaryTree<>());
+
+        BinaryTree oneline = new BinaryTree<Person>(new Person("Ruwwe", "Henning", 'M'),new BinaryTree<Person>(new Person("Ruwwe", "Annette",'W')),new BinaryTree<Person>(new Person("Ruwwe", "Jonas",'M')));
+
+        BinaryTree helene = new BinaryTree<Person>(new Person("Ruwwe","Helene",'W'));
+        BinaryTree volker = new BinaryTree<Person>(new Person("Ruwwe","Volker",'M'));
+        BinaryTree ursel = new BinaryTree<Person>(new Person("Degering","Ursel",'W'));
+        BinaryTree klaus = new BinaryTree<Person>(new Person("Degering","Klaus",'M'));
+        BinaryTree jonas = new BinaryTree<Person>(new Person("Ruwwe", "Jonas",'M'),helene, volker);
+        BinaryTree annette = new BinaryTree<Person>(new Person("Ruwwe","Annette",'W'),ursel, klaus);
+        BinaryTree henning = new BinaryTree<Person>(new Person("Ruwwe", "Henning",'M'),annette,jonas);
+
+        Ahnentafel test = new Ahnentafel(henning);
+        System.out.println(test.postOrder(test.getAhnenbaum()));
     }
 }
